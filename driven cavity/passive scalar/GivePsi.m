0fcb485267c7ccd0f173962bece2fb7258b2392d
Raffaele Riccardi <raffaele.riccardi01@gmail.com>
2026-05-11 12:17:42 +0200

    Update GivePsi.m

--- driven cavity/passive scalar/GivePsi.m
+++ driven cavity/passive scalar/GivePsi.m
@@ -1,34 +1,3 @@
-% function PSI = GivePsi(U,V)
-% 
-% % Function per il calcolo della funzione di corrente Psi per un dato campo
-% % di velocita' staggerato alla Harlow-Welch
-% % La funzione di corente e' ottenuta calcolando preliminarmente il campo di
-% % vorticità e risolvendo quindi una equazione di Poisson
-% 
-% global h LapPsi
-% [Nx,~] = size(U);           [~,Ny] = size(V);
-% Uy     = nan(Nx,Ny);        Vx     = nan(Nx,Ny);
-% % Calcolo delle derivate di velocita'
-% i = 2:Nx-1;  j = 2:Ny-1;
-% Uy(i,j) = (U(i,j)-U(i,j-1))/h;
-% Vx(i,j) = (V(i,j)-V(i-1,j))/h;
-% % Calcolo del campo di Zita dentro al dominio
-% ZITA = Uy - Vx;             ZITA = ZITA(2:end-1,2:end-1);
-% % Risoluzione della equazione di Poisson
-% zita = ZITA(:);
-% Psi  = LapPsi\zita;
-% % Agginta delle BCs
-% PSI  = reshape(Psi,Nx-2,Ny-2);
-% PSI  = [zeros(1,Nx-2);PSI;zeros(1,Nx-2)];
-% PSI  = [zeros(Ny,1),PSI,zeros(Ny,1)];
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% end
 
 function PSI = GivePsi(U,V)
 
